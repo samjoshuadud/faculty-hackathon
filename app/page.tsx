4d7a64ea5ccc89e999e0e24baa2d366df07a5bb7
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2025-05-06 12:47:23 +0800

    Cleaned

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,8 @@ import {useSession, signIn} from "next-auth/react";
 
 export default function Home() {
   const {data: session, status} = useSession();
-  if (status === 'loading') {
-    return <p>Loading</p>;
-  }
-
-  if (status === 'authenticated') {
-    return <p>Signed in as {session.user?.email}</p>;
-  }
   return (
   <>
-     <Button onClick={() => signIn("google")}>
-        Click Here!
-     </Button>
   </>
   );
 }
