9ddc61710254299ecc4531fd3f4dd08712fb22db
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2025-05-06 20:08:01 +0800

    Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,8 @@
 import {useSession} from 'next-auth/react';
 import {redirect} from 'next/navigation';
 import LoadingScreen from '../components/LoadingScreen'
+import {House, BookOpen, Award, ClipboardMinus, CalendarSearch} from 'lucide-react';
+
 export default function Home() {
   const {data: session, status} = useSession();
   if (status == 'loading') return <LoadingScreen />;
@@ -15,18 +17,19 @@ export default function Home() {
   const buttons = [
     {
       name: "My Dashboard",
+      icon: <House />
     },
     {
       name: "Education",
+      icon: <BookOpen />
     },
     {
       name: "Awards",
-    },
-    {
-      name: "Reports",
+      icon: <Award />
     },
     {
       name: "Events",
+      icon: <CalendarSearch />
     },
   ];
   return (
@@ -34,8 +37,9 @@ export default function Home() {
       <div className="border-r-1 border-white/20 h-full p-8 w-68 font-bold text-[#95D5B2] flex justify-center">
         <ul className="flex flex-col w-full">
           {buttons.map((button, index) => (
-            <li key={index}>
-              <button className="block w-full p-6 text-left cursor-pointer hover:bg-[#2D6A4F] rounded-sm transition-colors duration-200 ease-in-out">
+            <li key={index} className="">
+              <button className="flex items-center gap-4 block w-full p-6 text-left cursor-pointer hover:bg-[#2D6A4F] rounded-sm transition-colors duration-200 ease-in-out">
+              {button.icon}
                 {button.name}
               </button>
             </li>
